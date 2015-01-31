5eae62f2d7d63717e363fea8f97fc12fd6bd8136
Nathan Burgess <nathan.burgess28@hotmail.com>
2015-01-31 14:18:02 -0500

    Added state array

--- app/models/brewery_loader.rb
+++ app/models/brewery_loader.rb
@@ -1,14 +1,33 @@
 class BreweryLoader
 
+
+@states = ["Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Mississippi", "Missouri", "Montana", "Nebraska" , "Nevada", "New Hampshire", "New Jersey","New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"]
+
+
+
+
+
 Dotenv.load
 
-brewery_db = BreweryDB::Client.new do |config|
-  config.api_key = ENV['BREWERY_DB_API_TOKEN']
+def initialize
+  @brewery_db = BreweryDB::Client.new do |config|
+    config.api_key = ENV['BREWERY_DB_API_TOKEN']
+  end
 end
 
-san_fran = brewery_db.locations.all(locality: 'San Francisco')
+def num_breweries(state)
+  @brewery_db.locations.all(region: state ).count
+end
 
-binding.pry
+
+def build_state_hash(states)
+  hash = {}
+  states.each do |state|
+    hash[state] = num_breweries(state)
+  end
+end
+
+
 
 
 
