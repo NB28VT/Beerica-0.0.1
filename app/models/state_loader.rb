0a8f6b6fd4febd6cc5c21f142871783c63ef069c
Nathan Burgess <nathan.burgess28@hotmail.com>
2015-02-04 16:39:33 -0500

    Started working on seeder/loader/methods

--- app/models/state_loader.rb
+++ app/models/state_loader.rb
@@ -1,4 +1,4 @@
-class StateAndBreweryLoader
+class StateLoader
 
   def initialize
     @states = ["Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "District of Columbia", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska" , "Nevada", "New Hampshire", "New Jersey","New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"]
@@ -8,22 +8,7 @@ class StateAndBreweryLoader
     @states.each do |state|
       state = State.new(state)
       state.save
+    end
   end
-
-
-
-
-
   
-
-
-
-
-
-
-
-
-
-
-
 end
