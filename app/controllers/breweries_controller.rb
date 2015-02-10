da58751702737a30d5a71357675a50b9c37b86dc
Nathan Burgess <nathan.burgess28@hotmail.com>
2015-02-10 16:30:38 -0500

    Succesfully hooked up google test map for state brewery index page

--- app/controllers/breweries_controller.rb
+++ app/controllers/breweries_controller.rb
@@ -1,13 +1,10 @@
-require 'dotenv-rails'
+require 'dotenv'
+Dotenv.load
+
 class BreweriesController < ApplicationController
-
-
   def index
-    Dotenv.load
-
-    api_key = ENV['GOOGLE_MAP_API_KEY_TOKEN']
+    api_key = ENV['GOOGLE_MAP_API_TOKEN']
     @map_api_call = "https://maps.googleapis.com/maps/api/js?key=#{api_key}"
-    binding.pry
     @state = State.find(params[:state_id])
     @breweries = @state.breweries
   end
