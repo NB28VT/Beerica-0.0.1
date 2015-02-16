6af44651516cc121bdca51eea72100d255589dea
Nathan Burgess <nathan.burgess28@hotmail.com>
2015-02-16 17:10:04 -0500

    Added location hash builder for states

---
+++ app/models/build_location_array.rb
@@ -0,0 +1,17 @@
+class BuildLocationArray
+
+  def build_locations(breweries)
+    location_array = []
+
+    breweries.each do |brewery|
+      brewery_array = []
+      brewery_array.push(
+        brewery.name,
+        brewery.latitude,
+        brewery.longitude
+      )
+      location_array << brewery_array
+    end
+    location_array
+  end
+end
