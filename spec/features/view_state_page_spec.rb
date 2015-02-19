d9c85267b0139bb8207f76e7d574c0c96c4b3e6d
Nathan Burgess <nathan.burgess28@hotmail.com>
2015-02-18 21:33:52 -0500

    Added test to find links to all breweries on a state page

--- spec/features/view_state_page_spec.rb
+++ spec/features/view_state_page_spec.rb
@@ -6,14 +6,20 @@ I would like to select a state
 So I can see a list of all the breweries in that state
 
 Acceptance criteria:
-[x] A user can visit a state and be brought to a state info page (untested in this suite)
-[x] The page will include links to brewery pages for each brewery
-[ ] The page includes a Google map for the state, with all brewery locations
+[x] A user can visit a state and be brought to a state info page (JS is untested in this suite)
+[ ] The state page includes links to all breweries in the state
+[x] A user can click on a brewery and get information on the brewery
+[x] The state page includes a Google map for the state, with all brewery locations marked (JS is untested in this suite)
 "
 ) do
 
   state_id = State.find_by(name: "Vermont").id
   brewery = Brewery.find_by(name: "Rock Art Brewery")
+  all_breweries = []
+
+  Brewery.where(state_id: state_id).each do |brewery|
+    all_breweries << brewery.name
+  end
 
   scenario "A user can visit a state info page", js: true do
     visit state_breweries_path(state_id)
@@ -21,13 +27,22 @@ Acceptance criteria:
     expect(page).to have_content("Vermont")
   end
 
-  scenario "The page will include URL links to brewery pages for each brewery" do
+  scenario "A user can click on a brewery and get information on the brewery" do
     visit state_breweries_path(state_id)
 
     click_on brewery.name
 
     expect(page).to have_content(brewery.website)
   end
+
+  scenario "The state page includes a Google map for the state, with all brewery locations marked", js: true do
+    visit state_breweries_path(state_id)
+
+    all_breweries.each do |brewery_name|
+      expect(page).to have_content(brewery_name)
+    end
+  end
+
 
 
 end
