65792a3fd19ab28b5de2b13743638d8c17f1c2d4
Nathan Burgess <nathan.burgess28@hotmail.com>
2015-02-26 10:08:45 -0500

    Switched city table in breweries to city id

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150226000616) do
+ActiveRecord::Schema.define(version: 20150226150052) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,7 +19,6 @@ ActiveRecord::Schema.define(version: 20150226000616) do
   create_table "breweries", force: :cascade do |t|
     t.string   "name",               null: false
     t.integer  "state_id",           null: false
-    t.string   "city"
     t.string   "street_address"
     t.string   "website"
     t.string   "phone"
@@ -28,6 +27,7 @@ ActiveRecord::Schema.define(version: 20150226000616) do
     t.datetime "updated_at",         null: false
     t.float    "latitude"
     t.float    "longitude"
+    t.integer  "city_id"
   end
 
   create_table "cities", force: :cascade do |t|
