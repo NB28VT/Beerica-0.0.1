62982b02c4080808ecb62d14d52c5b913441ddbe
Nathan Burgess <nathan.burgess28@hotmail.com>
2015-02-25 19:05:40 -0500

    Fixed wrong name in cities column. Called it cities initially

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150226000000) do
+ActiveRecord::Schema.define(version: 20150226000317) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,7 +31,7 @@ ActiveRecord::Schema.define(version: 20150226000000) do
   end
 
   create_table "cities", force: :cascade do |t|
-    t.string   "cities",     null: false
+    t.string   "name",       null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
