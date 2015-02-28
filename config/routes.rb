d95e38aa31428548f19d0dd491b23cf1b0e666f4
Nathan Burgess <nathan.burgess28@hotmail.com>
2015-02-28 11:47:40 -0500

    Added JS listener for city page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'dashboard#index'
   resources :dashboard, only: [:index]
   resources :states, only: [:index, :show] do
-    resources :breweries, only: [:index, :show, :redirect]
+    resources :breweries, only: [:index, :show, :redirect],
+    resources :cities, only: [:show]
   end
 end
