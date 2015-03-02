bda78de685298cedb7756a3e8e29cb202d9df6c2
Nathan Burgess <nathan.burgess28@hotmail.com>
2015-03-01 21:06:44 -0500

    Removed unused dashboard views, routes, and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   root 'states#index'
   resources :info, only: [:index]
-  resources :dashboard, only: [:index]
   resources :states, only: [:index, :show] do
     resources :breweries, only: [:index, :show, :redirect]
     resources :cities, only: [:show]
