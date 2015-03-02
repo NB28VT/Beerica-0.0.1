e7a862d43e918ba851465d0b46c159713900ccf7
Nathan Burgess <nathan.burgess28@hotmail.com>
2015-03-02 13:18:15 -0500

    Reconfigured icon bar to link through rails link_to helper

--- spec/features/navigate_top_bar_spec.rb
+++ spec/features/navigate_top_bar_spec.rb
@@ -13,22 +13,22 @@ Acceptance criteria:
 
   state_id = State.find_by(name: "Vermont").id
 
-  scenario "The home page link returns the user to the state index page" do
+  scenario "The home page link returns the user to the state index page", js: true do
     visit state_breweries_path(state_id)
 
     click_on "Home"
     expect(page).to have_content("Breweries per capita:")
   end
 
-  scenario "The search link brings the user to a search for breweries page" do
+  scenario "The search link brings the user to a search for breweries page", js: true do
 
   end
 
-  scenario "The share link lets the user share Beerica via Twitter and Facebook" do
+  scenario "The share link lets the user share Beerica via Twitter and Facebook", js: true do
 
   end
 
-  scenario "The info page link brings the user to an info page" do
+  scenario "The info page link brings the user to an info page", js: true do
 
   end
 
