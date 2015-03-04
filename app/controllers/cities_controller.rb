f9b230ccded65c55f4db70482683526b807ab450
Nathan Burgess <nathan.burgess28@hotmail.com>
2015-03-04 17:51:37 -0500

    Fixed problem with map Div. Moving forward with city page styling

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -3,7 +3,7 @@ class CitiesController < ApplicationController
   def show
     @city = City.find(params[:id])
     @breweries = @city.breweries
+    location_builder = BuildLocationArray.new
+    @brewery_location_array = location_builder.build_locations(@breweries).to_json
   end
-
-
 end
