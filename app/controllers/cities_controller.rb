89048dde759483badfa71e4c4455a5056c278a9f
Nathan Burgess <nathan.burgess28@hotmail.com>
2015-03-04 19:26:22 -0500

    Sorted city dropbdown alphbetically

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -2,7 +2,7 @@ class CitiesController < ApplicationController
 
   def show
     @city = City.find(params[:id])
-    @breweries = @city.breweries
+    @breweries = @city.breweries.sort_by &:name
     location_builder = BuildLocationArray.new
     @brewery_location_array = location_builder.build_locations(@breweries).to_json
   end
