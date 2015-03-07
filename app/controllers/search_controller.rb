1247ca39bfce99a13045c0dcf1ce2067e8c23623
Nathan Burgess <nathan.burgess28@hotmail.com>
2015-03-06 19:26:23 -0500

    Basic search functionality with sidekick works. Moving forward to styling

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,7 @@
 class SearchController < ApplicationController
   def index
-
+    if params[:query].present?
+      @brewery_results = Brewery.search(params[:query], page: params[:page])
+    end
   end
 end
