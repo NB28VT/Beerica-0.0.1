350a5b1dc3310423abf12bf233f23c4a20f055d0
Nathan Burgess <nathan.burgess28@hotmail.com>
2015-03-10 13:44:27 -0400

    Added user breweries join table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150309154100) do
+ActiveRecord::Schema.define(version: 20150310173938) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,13 @@ ActiveRecord::Schema.define(version: 20150309154100) do
     t.datetime "created_at",           null: false
     t.datetime "updated_at",           null: false
     t.float    "breweries_per_capita", null: false
+  end
+
+  create_table "user_breweries", force: :cascade do |t|
+    t.integer  "user_id",    null: false
+    t.integer  "brewery_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
